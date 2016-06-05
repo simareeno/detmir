7df06f6a7173675fa7a13bd690e8dbff6099ffb7
simareeno <sima@chetverukhin.ru>
2016-06-05 16:39:34 +0400

    Added filter example

--- tips/template-mustache.js
+++ tips/template-mustache.js
@@ -19,23 +19,47 @@ function sortByName(a, b){
     return 0;
 }
 
+function filterBy(property, keyword, e) {
+
+	return e.color === keyword;
+
+}
+
 $(document).ready(function () {
 	show(initialArray); // сначала загружаем наш изначальный массив
 	var changedArray = initialArray.slice(); // делаем копию
 
 	// сортировка
-	$('.select').change(function(){
+	$('.select').change(function() {
 
-		if ($(this).val() == 'name') {
+
+		if ($(this).val() === 'name') {
 			changedArray.sort(sortByName);
-		} else if ($(this).val() == 'price') {
+		} else if ($(this).val() === 'price') {
 			changedArray.sort(sortByPrice);
 		} else if($(this).val() === 'relevance') {
 			show(initialArray);
 			return false;
 		}
 
-	show(changedArray); // показываем измененный массив
+		show(changedArray); // показываем измененный массив
+	});
+
+	// фильтр по цвету
+
+	$('.color').change(function() {
+		var changedArray = initialArray.slice(); // делаем копию
+
+		if ($(this).val() === 'yellow') {
+			changedArray = changedArray.filter(filterBy.bind(undefined, 'color', 'yellow'));
+		} else if ($(this).val() === 'orange') {
+			changedArray = changedArray.filter(filterBy.bind(undefined, 'color', 'orange'));
+		} else if($(this).val() === 'color') {
+			show(initialArray);
+			return false;
+		}
+
+		show(changedArray); // показываем измененный массив
 	});
 });
 
@@ -43,13 +67,19 @@ $(document).ready(function () {
 var initialArray = [
 	{
 		"title": "Яблоко",
-		"price": 2200
+		"price": 2200,
+		'fruit': true,
+		"color": 'green'
 	}, {
 		"title": "Банан",
-		"price": 2500
+		"price": 2500,
+		"frut": true,
+		"color": 'yellow'
 	}, {
 		"title": "Морковка",
 		"price": 1950,
+		"fruit": false,
+		"color": 'orange',
 		"sale": 'item--sale'
 	}
 ];
